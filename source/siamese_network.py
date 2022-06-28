c9aa670b10da399ef6de19851d858347a9998933
David Walser <david.walser@techtalk.at>
2022-06-28 15:33:03 +0200

    added two tests with 100 epoch each

--- source/siamese_network.py
+++ source/siamese_network.py
@@ -6,6 +6,7 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import GlobalAveragePooling2D
 from tensorflow.keras.layers import MaxPooling2D
+from keras.utils.vis_utils import plot_model
 
 def build_siamese_model(inputShape, embeddingDim=48):
 	# specify the inputs for the feature extractor network
@@ -14,23 +15,22 @@ def build_siamese_model(inputShape, embeddingDim=48):
 	x = Conv2D(64, (2, 2), padding="same", activation="relu")(inputs)
 	x = MaxPooling2D(pool_size=(2, 2))(x)
 	x = Dropout(0.3)(x)
-	# second set of CONV => RELU => POOL => DROPOUT layers
-	# x = Conv2D(64, (2, 2), padding="same", activation="relu")(x)
-	# x = MaxPooling2D(pool_size=2)(x)
-	# x = Dropout(0.3)(x)
-	
-	# x = Conv2D(64, (2, 2), padding="same", activation="relu")(x)
-	# x = MaxPooling2D(pool_size=2)(x)
-	# x = Dropout(0.3)(x)
 
-	# x = Conv2D(64, (2, 2), padding="same", activation="relu")(x)
-	# x = MaxPooling2D(pool_size=2)(x)
-	# x = Dropout(0.3)(x)
+	# second set of CONV => RELU => POOL => DROPOUT layers
+	x = Conv2D(64, (2, 2), padding="same", activation="relu")(x)
+	x = MaxPooling2D(pool_size=2)(x)
+	x = Dropout(0.3)(x)
+
+	x = Conv2D(64, (2, 2), padding="same", activation="relu")(x)
+	x = MaxPooling2D(pool_size=2)(x)
+	x = Dropout(0.3)(x)
+	
 	# prepare the final outputs
 	pooledOutput = GlobalAveragePooling2D()(x)
 	outputs = Dense(embeddingDim)(pooledOutput)
 	# build the model
 	model = Model(inputs, outputs)
 	model.summary()
+	plot_model(model, to_file='model.png')
 	# return the model to the calling function
 	return model

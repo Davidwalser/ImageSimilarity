afbaf0beb46903f8476364fffc883365f9229fb7
davidwalser <walser.dave@gmail.com>
2022-07-19 18:27:41 +0200

    refactored sift

--- source/sift.py
+++ source/sift.py
@@ -19,31 +19,30 @@ def calculateMatches(des1,des2):
         topResults = []
         for match1 in topResults1:
             match1QueryIndex = match1[0].queryIdx
-            match1TrainIndex = match1[0].trainId
+            match1TrainIndex = match1[0].trainIdx
             for match2 in topResults2:
                 match2QueryIndex = match2[0].queryIdx
-                match2TrainIndex = match2[0].trainId
+                match2TrainIndex = match2[0].trainIdx
                 if (match1QueryIndex == match2TrainIndex) and (match1TrainIndex == match2QueryIndex):
                     topResults.append(match1)
         return topResults
 
 def get_sift_similarity_score(filename1, filename2, plot_result = False):
     try:
-        img1=cv2.imread(str(filename1),4)
-        img2=cv2.imread(str(filename2),4)
+        img1=cv2.imread(str(filename1),cv2.IMREAD_ANYCOLOR)
+        img2=cv2.imread(str(filename2),cv2.IMREAD_ANYCOLOR)
 
-        # Initiate SURF detector
         sift=cv2.SIFT_create()
 
-        # find the keypoints and descriptors with SIFT
-        kp1, des1 = sift.detectAndCompute(img1,None)
-        kp2, des2 = sift.detectAndCompute(img2,None)
+        kp1, des1 = sift.detectAndCompute(img1, None)
+        kp2, des2 = sift.detectAndCompute(img2, None)
+
+        good_matches = calculateMatches(des1,des2)
 
         if(plot_result):
             img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good_matches,None,flags=2)
-            plt.imshow(img3),plt.show()
-        
-        good_matches = calculateMatches(des1,des2)
+            plt.imshow(cv2.cvtColor(img3, cv2.COLOR_BGR2RGB)),plt.show()
+
         if good_matches is None:
             return 0
         score = 100 * (len(good_matches)/min(len(kp1),len(kp2)))
@@ -51,5 +50,10 @@ def get_sift_similarity_score(filename1, filename2, plot_result = False):
     except:
         return 0
 
-# get_sift_similarity_score("C:\Study\FH_Campus\MasterThesis\source\Images\\2004-06071-2004-08279\\2004-06316.gif", 
-# "C:\Study\FH_Campus\MasterThesis\source\Images\\2004-06071-2004-08279\\2004-06317.gif")
+# get_sift_similarity_score("C:\Study\FH_Campus\MasterThesis\source\SurveyPictures\\test.gif", 
+# "C:\Study\FH_Campus\MasterThesis\source\SurveyPictures\\test3.gif",
+# True)
+
+# get_sift_similarity_score("C:\Study\FH_Campus\MasterThesis\source\SurveyPictures\sift_test1.gif", 
+# "C:\Study\FH_Campus\MasterThesis\source\SurveyPictures\sift_test2.gif",
+# True)
